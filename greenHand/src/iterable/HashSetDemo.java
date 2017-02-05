da3e6248288dda566dc18e2bcbe501be3f9256ce
faddei <gaofeijian9@foxmail.com>
2017-02-05 22:21:01 +0800

    hashset的使用

---
+++ greenHand/src/iterable/HashSetDemo.java
@@ -0,0 +1,17 @@
+package iterable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+    public static void main(String[] args)
+    {
+        Set<String> set = new HashSet<>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("D");
+        set.add("A");
+        System.out.println(set);
+    }
+}
